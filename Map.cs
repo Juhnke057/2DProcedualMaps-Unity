6f7659fafb90ae10d0497fc090c900a5f098e5b4
Viktor Juhnke <57365322+Juhnke057@users.noreply.github.com>
2021-04-23 16:40:30 +0200

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum MapDisplay
+{
+    Height,
+    Moisture,
+    Heat,
+    Biome
+}
+
+[ExecuteInEditMode]
+public class Map : MonoBehaviour
+{
+    public MapDisplay displayType;
+
+    public RawImage debugImage;
+
+    public BiomePreset[] biomes;
+
+    public GameObject tilePrefab;
+
+    [Header("Dimensions")]
+    public int width;
+    public int height;
+    public float scale;
+    public Vector2 offset;
+
+    [Header("Height Map")]
+    public Wave[] heightWaves;
+    public Gradient heightDebugColors;
+    public float[,] heightMap;
+
+    [Header("Moisture Map")]
+    public Wave[] moistureWaves;
+    public Gradient moistureDebugColors;
+    public float[,] moistureMap;
+
+    [Header("Heat Map")]
+    public Wave[] heatWaves;
+    public Gradient heatDebugColors;
+    public float[,] heatMap;
+
+
+    private float lastGenerateTime;
+
+    private void Start()
+    {
+        if (Application.isPlaying)
+        GenerateMap();
+    }
+
+    private void Update()
+    {
+        if (Application.isPlaying)
+            return;
+        if(Time.time - lastGenerateTime > 0.1f)
+        {
+            lastGenerateTime = Time.time;
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        heightMap = NoiseGenerator.Generate(width, height, scale, offset, heightWaves);
+
+        moistureMap = NoiseGenerator.Generate(width, height, scale, offset, moistureWaves);
+
+        heatMap = NoiseGenerator.Generate(width, height, scale, offset, heatWaves);
+
+        Color[] pixels = new Color[width * height];
+        int i = 0;
+
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                switch (displayType)
+                {
+                    case MapDisplay.Height:
+                        pixels[i] = heightDebugColors.Evaluate(heightMap[x, y]);
+                        break;
+                    case MapDisplay.Moisture:
+                        pixels[i] = moistureDebugColors.Evaluate(moistureMap[x, y]);
+                        break;
+                    case MapDisplay.Heat:
+                        pixels[i] = heatDebugColors.Evaluate(heatMap[x, y]);
+                        break;
+                    case MapDisplay.Biome:
+                        {
+                            BiomePreset biome = GetBiome(heightMap[x, y], moistureMap[x, y], heatMap[x, y]);
+                            pixels[i] = biome.debugColor;
+
+
+                            if (Application.isPlaying)
+                            {
+                                GameObject tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity);
+                                tile.GetComponent<SpriteRenderer>().sprite = biome.GetTileSprite();
+
+                            }
+
+                            break;
+
+
+                        }
+                }
+
+                i++;
+            }
+        }
+        Texture2D tex = new Texture2D(width, height);
+        tex.SetPixels(pixels);
+        tex.filterMode = FilterMode.Point;
+        tex.Apply();
+
+        debugImage.texture = tex;
+    }
+    BiomePreset GetBiome(float height, float moisture, float heat)
+    {
+        BiomePreset biomeToReturn = null;
+        List<BiomePreset> tempBiomes = new List<BiomePreset>();
+
+        foreach(BiomePreset biome in biomes)
+        {
+            if(biome.MatchCondition(height, moisture, heat))
+            {
+                tempBiomes.Add(biome);
+            }
+        }
+
+        float curValue = 0.0f;
+
+        foreach(BiomePreset biome in tempBiomes)
+        {
+            float diffValue = (height - biome.minHeight) + (moisture - biome.minMoisture) + (heat - biome.minHeat);
+
+            if(biomeToReturn == null)
+            {
+                biomeToReturn = biome;
+                curValue = diffValue;
+
+            }
+            else if(diffValue < curValue)
+            {
+                biomeToReturn = biome;
+                curValue = diffValue;
+            }
+
+        }
+        if(biomeToReturn == null)
+        {
+            return biomes[0];
+        }
+
+        return biomeToReturn;
+    }
+
+}
+
---
+++ NoiseGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour
+{
+    public static float[,] Generate (int width, int height, float scale, Vector2 offset, Wave[] waves)
+    {
+        float[,] noiseMap = new float[width, height];
+        
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                // Calc. Sample Pos.
+                float samplePosX = (float)x * scale + offset.x;
+                float samplePosY = (float)y * scale + offset.y;
+
+                float normalization = 0.0f;
+
+                foreach(Wave wave in waves)
+                {
+                    float waveSamplePosX = samplePosX * wave.frequency + wave.seed;
+                    float waveSamplePosY = samplePosY * wave.frequency + wave.seed;
+
+                    noiseMap[x, y] += wave.amplitude * Mathf.PerlinNoise(waveSamplePosX, waveSamplePosY);
+                    normalization += wave.amplitude;
+                }
+                noiseMap[x, y] /= normalization;
+            }
+        }
+
+        return noiseMap;
+
+    }
+}
+
+[System.Serializable]
+public class Wave
+{
+
+    public float seed;
+    public float frequency;
+    public float amplitude;
+
+}
